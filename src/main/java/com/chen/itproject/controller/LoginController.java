0c3540760d8a0a802944405e1d0ca3b3643d0aaf
lester.chen <suyuc@student.unimelb.edu.au>
2024-09-11 18:40:21 +1000

    [TASK-169] <<added>> realize login/out and sign up

---
+++ src/main/java/com/chen/itproject/controller/LoginController.java
@@ -0,0 +1,51 @@
+package com.chen.itproject.controller;
+
+
+import com.chen.itproject.pojo.Result;
+import com.chen.itproject.pojo.User;
+import com.chen.itproject.services.UsersService;
+import com.chen.itproject.util.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RequestMapping("/login")
+@RestController
+public class LoginController {
+
+    @Autowired
+    UsersService usersService;
+
+    @PostMapping
+    public Result login(@RequestBody User user){
+
+        User userFound = usersService.login(user);
+
+        if (userFound != null) {
+            HashMap<String, Object> claims = new HashMap<>();
+            claims.put("id", userFound.getUserId());
+            claims.put("name", userFound.getName());
+            String jwt = JwtUtils.createJwt(claims);
+
+            return Result.successResult(jwt);
+        } else {
+            return Result.errorResult("NOT_LOGIN");
+        }
+    }
+
+    @PutMapping
+    public Result create(@RequestBody User user) {
+
+        int code = usersService.signUp(user);
+
+        if (code == 1) {
+            return Result.successResult(null);
+        } else {
+            return Result.errorResult();
+        }
+    }
+
+}
