afbb5d007d76f422a9dbe67e7b38fced2e7e27fc
lester.chen <suyuc@student.unimelb.edu.au>
2024-09-17 08:37:14 +1000

    [TASK-171] <<added>> entity class of user

---
+++ src/main/java/com/chen/itproject/pojo/User.java
@@ -0,0 +1,14 @@
+package com.chen.itproject.pojo;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+
+@Data
+public class User {
+    Integer userId;
+    String name;
+    String password;
+    String emblem;
+    Integer rewardInfo;
+}
