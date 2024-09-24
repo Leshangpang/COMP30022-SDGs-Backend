ac4ef4f79eee79b7fad67ca76fa689a91811dfb8
opang <111403917+Leshangpang@users.noreply.github.com>
2024-09-24 17:06:16 +1000

    [TASK-230] <<added>> integrated test

---
+++ src/test/java/com/chen/itproject/IntegratedTestIT.java
@@ -0,0 +1,12 @@
+package com.chen.itproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IntegratedTestIT {
+
+    @Test
+    public void test() throws Exception {
+    }
+}
